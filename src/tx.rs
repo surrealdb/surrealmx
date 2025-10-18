f9d033505fcee2a739e4399cedcba12d02832c11
Tobie Morgan Hitchcock <tobie@surrealdb.com>
2025-10-18 12:14:03 +0100

    Ensure transaction merge queue is checked for entries

--- src/tx.rs
+++ src/tx.rs
@@ -1179,6 +1179,19 @@ where
 	where
 		Q: Borrow<K>,
 	{
+		// Fetch the transaction merge queue range
+		let iter = self.database.transaction_merge_queue.range(..=version);
+		// Check the current entry iteration
+		for entry in iter.rev() {
+			// There is a valid merge queue entry
+			if !entry.is_removed() {
+				// Check if the entry has a key
+				if let Some(v) = entry.value().writeset.get(key.borrow()) {
+					// Return the entry value
+					return v.as_ref().map(|arc| arc.as_ref().clone());
+				}
+			}
+		}
 		// Check the key in the datastore
 		self.database
 			.datastore
@@ -1193,6 +1206,19 @@ where
 	where
 		Q: Borrow<K>,
 	{
+		// Fetch the transaction merge queue range
+		let iter = self.database.transaction_merge_queue.range(..=version);
+		// Check the current entry iteration
+		for entry in iter.rev() {
+			// There is a valid merge queue entry
+			if !entry.is_removed() {
+				// Check if the entry has a key
+				if let Some(v) = entry.value().writeset.get(key.borrow()) {
+					// Return whether the entry exists
+					return v.is_some();
+				}
+			}
+		}
 		// Check the key in the datastore
 		self.database
 			.datastore
@@ -1207,6 +1233,22 @@ where
 	where
 		Q: Borrow<K>,
 	{
+		// Fetch the transaction merge queue range
+		let iter = self.database.transaction_merge_queue.range(..=version);
+		// Check the current entry iteration
+		for entry in iter.rev() {
+			if !entry.is_removed() {
+				// Check if the entry has a key
+				if let Some(v) = entry.value().writeset.get(key.borrow()) {
+					// Return whether the entry matches
+					return match (chk.as_ref(), v.as_ref()) {
+						(Some(x), Some(y)) => x == y.as_ref(),
+						(None, None) => true,
+						_ => false,
+					};
+				}
+			}
+		}
 		// Check the key in the datastore
 		match (
 			chk.as_ref(),
